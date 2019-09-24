a14c8cab061efc1a3ae45eb9c03b43c30dfe94ee
skytechstretch <55679788+skytechstretch@users.noreply.github.com>
2019-09-24 10:56:32 +0530

    Update flow-0.js

--- flow-0.js
+++ flow-0.js
@@ -1,14 +1,1 @@
 
-why typescript ?  4 min 
-
-what new Power ? 3 min 
-
-What are the arrtibutes for its new power ? 4 min 
-
-What is type script ? 3 min 
-
-
-14 min 
-----------------
-
-In which kind of Allication we can involve typescript ? 2 min
